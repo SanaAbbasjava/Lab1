a07842fe3f1d8d9f34917ca6a93a7b9e02d6afe6
Sana Abbas <sana.abbs@gmail.com>
2026-08-30 22:13:27 -0700

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * This is my first java app printing out hello.
+ * World to the console.
+ * 
+ * @author (Sana Abbas), sabbas@student.sdccd.edu
+ * @Since 8/30/26
+ * @version 21.0.6
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("Sana Abas");
+    
+    } 
+
+}
